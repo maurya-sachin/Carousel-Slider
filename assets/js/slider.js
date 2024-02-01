fd3e5833fcf0582343b2101bcd644636b36c9f3e
sachin-maurya <sachinmaurya1710@gmail.com>
2024-02-01 16:44:31 +0530

    added code for dynamicaly adding background in slide

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -3,8 +3,75 @@ let wrapper = document.querySelector('.wrapper');
 let next = document.querySelector('.arrow-next');
 let prev = document.querySelector('.arrow-prev');
 let slide = document.querySelectorAll('.slide');
+let background = document.querySelector('.background');
 let currdeg = 0;
 let active = 0;
+
+const sliderBackground = {
+    "slide1": {
+        "id": "slide1",
+        "src": "/assets/images/slider1.jpg"
+    },
+    "slide2": {
+        "id": "slide2",
+        "src": "/assets/images/slider2.jpg"
+    },
+    "slide3": {
+        "id": "slide3",
+        "src": "/assets/images/slider3.jpg"
+    },
+    "slide4": {
+        "id": "slide4",
+        "src": "/assets/images/slider4.jpg"
+    },
+    "slide5": {
+        "id": "slide5",
+        "src": "/assets/images/slider5.jpg"
+    },
+    "slide6": {
+        "id": "slide6",
+        "src": "/assets/images/slider6.jpg"
+    }
+};
+
+const preloadSlideBackgrounds = () => {
+    for (const slideId in sliderBackground) {
+        const slideBackground = sliderBackground[slideId].src;
+        const img = new Image();
+        img.src = slideBackground;
+        document.getElementById(slideId).style.backgroundImage = `url(${slideBackground})`;
+    }
+    updateBackgrounds();
+};
+
+const updateBackgrounds = () => {
+    const activeSlideId = `slide${active + 1}`;
+    const activeSlideBackground = sliderBackground[activeSlideId].src;
+
+    slide.forEach(slide => {
+        slide.classList.remove('active');
+    });
+
+    const currentSlide = document.getElementById(activeSlideId);
+    if (currentSlide) {
+        currentSlide.classList.add('active');
+    }
+
+    setTimeout(() => {
+        background.style.transition = 'background-image 0.5s ease';
+        background.style.backgroundImage = `url(${activeSlideBackground})`;
+    }, 100);
+
+    setTimeout(() => {
+        slider.style.transition = '';
+    }, 600);
+};
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    preloadSlideBackgrounds();
+});
 
 next.addEventListener('click', () => {
     slider.classList.toggle('zoom');
@@ -18,6 +85,7 @@ next.addEventListener('click', () => {
     }
 
     toggle();
+    updateBackgrounds();
 });
 
 prev.addEventListener('click', () => {
@@ -32,7 +100,9 @@ prev.addEventListener('click', () => {
     }
 
     toggle();
+    updateBackgrounds();
 });
+
 
 let toggle = () => {
     setTimeout(() => {
